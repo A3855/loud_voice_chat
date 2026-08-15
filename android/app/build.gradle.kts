b0150fe17f6b4d8a958564938e375860d6ec5a9b
Akash boss <119699654+A3855@users.noreply.github.com>
2026-08-15 12:24:02 +0600

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "com.example.loud_voice_chat"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = 21
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
